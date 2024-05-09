c677dd57e910611e68c48f88925ed2260e50de00
Nacho Betancourt <trsgarp@gmail.com>
2024-05-09 01:39:21 -0600

    logo

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import LogoIcon from './icons/logo';
+import Image from 'next/image';
 
 export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
   return (
@@ -12,12 +12,7 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
         }
       )}
     >
-      <LogoIcon
-        className={clsx({
-          'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
-        })}
-      />
+      <Image src="/images/gym.svg" alt="logo" width={16} height={10} />
     </div>
   );
 }
